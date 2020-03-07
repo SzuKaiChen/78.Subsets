1e01923249e6d85cb9cb784c52127bcac1971950
SzuKaiChen <57374287+SzuKaiChen@users.noreply.github.com>
2020-03-06 21:53:39 -0600

    Create DFS.py

---
+++ 78.Subsets/DFS.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+        if not nums: return res
+        self.dfs(nums, 0, [], res)
+        return res
+    
+    def dfs(self, nums, index, path, res):
+        res.append(path)
+        for i in range(index, len(nums)):
+            self.dfs(nums, i+1, path+[nums[i]], res)
+        
+        
