59b7ed407a9e8a74e79a9351ed9c04c09683b922
SzuKaiChen <57374287+SzuKaiChen@users.noreply.github.com>
2020-03-31 23:10:25 -0500

    Create DFS.py

---
+++ 897.Increasing Order Search Tree/DFS.py
@@ -0,0 +1,32 @@
+def increasingBST(self, root, tail = None):
+        if not root: return tail
+        res = self.increasingBST(root.left, root)
+        root.left = None
+        root.right = self.increasingBST(root.right, tail)
+        return res
+        
+#based on lee215@leetcode
+#he gave the inspired solution for this issue.
+#here is his comment
+#Recursively call function increasingBST(TreeNode root, TreeNode tail)
+#tail is its next node in inorder,
+#（the word next may be easier to understand, but it’s a keyword in python)
+
+#If root == null, the head will be tail, so we return tail directly.
+
+#we recursively call increasingBST(root.left, root),
+#change left subtree into the linked list + current node.
+
+#we recursively call increasingBST(root.right, tail),
+#change right subtree into the linked list + tail.
+
+#Now the result will be in a format of linked list, with right child is next node.
+#Since it's single linked list, so we set root.left = null.
+#Otherwise it will be TLE for Leetcode judgment to traverse over your tree.
+
+#The result now is increasingBST(root.left) + root + increasingBST(root.right).
+
+#One tip here, we should arrange the old tree, not create a new tree.
+#The leetcode judgment comparer only the values,
+#so it won't take it as wrong answer if you return a new tree,
+#but it is wrong.
